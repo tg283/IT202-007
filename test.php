54232af15ff152bf153d3a10ac11d4ae71450de4
tg283 <tg283@njit.edu>
2020-09-21 23:19:59 -0400

    change test.php

--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php
 
-    echo "Deployment proof test file";
-    echo "URL of file on GitHub: https://github.com/tg283/IT202-007/blob/master/test.php"
+    echo "URL of this file on GitHub: https://github.com/tg283/IT202-007/blob/master/test.php"
 
 ?>
